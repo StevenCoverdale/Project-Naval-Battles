e0b0dce56672ce5a805dc83955485cab82024f80
yongedevil <briicely@gmail.com>
2015-01-16 08:48:27 -0500

    removed non-working unreal project

--- NavalBattles/Source/NavalBattles/Private/NavalBattles.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
-
-#include "NavalBattles.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NavalBattles, "NavalBattles" );
-
-DEFINE_LOG_CATEGORY(LogNavalBattles)
- 
