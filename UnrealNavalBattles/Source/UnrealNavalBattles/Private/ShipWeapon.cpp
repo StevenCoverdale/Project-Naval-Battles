fc93dd1c5595de943e3bbfdc6a10f751842ca5d3
bagenoxu <bagenoxu1337@hotmail.com>
2015-01-30 09:53:57 -0500

    Created weapon system and updated weapon class

--- UnrealNavalBattles/Source/UnrealNavalBattles/Private/ShipWeapon.cpp
+++ UnrealNavalBattles/Source/UnrealNavalBattles/Private/ShipWeapon.cpp
@@ -2,6 +2,7 @@
 
 #include "UnrealNavalBattles.h"
 #include "ShipWeapon.h"
+#include "ShipBullet.h"
 
 
 AShipWeapon::AShipWeapon(const class FPostConstructInitializeProperties& PCIP)
@@ -10,4 +11,38 @@ AShipWeapon::AShipWeapon(const class FPostConstructInitializeProperties& PCIP)
 
 }
 
+bool AShipWeapon::inRange()
+{
+	//if (target distance is within minRange and maxRange of weapon)
+	{
+		return true;
+	}
 
+	return false;
+}
+
+bool AShipWeapon::inFireArc()
+{
+	//if (target arc is between minimum arc and maximum arc)
+	{
+		return true;
+	}
+	return false;
+}
+
+void AShipWeapon::fireWeapon()
+{
+	if(ProjectileClass)
+	{
+		GetWorld()->SpawnActor<AShipBullet>(ProjectileClass), GetActorLocation(), GetActorRotation();
+		weaponAmmo--;
+	}
+}
+
+void AShipWeapon::reloadWeapon()
+{
+	if (weaponAmmo <= weaponAmmoMax)
+	{
+		weaponAmmo = weaponAmmoMax;
+	}
+}
--- UnrealNavalBattles/Source/UnrealNavalBattles/Public/ShipWeapon.h
+++ UnrealNavalBattles/Source/UnrealNavalBattles/Public/ShipWeapon.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/Pawn.h"
+#include "ShipBullet.h"
 #include "ShipWeapon.generated.h"
 
 /**
@@ -13,12 +14,29 @@ class UNREALNAVALBATTLES_API AShipWeapon : public APawn
 {
 	GENERATED_UCLASS_BODY()
 
-	int weaponRange;
-	int weaponDamage;
-	int weaponAmmo;
-	bool isWeaponArc;
-	bool isWeaponControllable;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Projectiles)
+	TSubclassOf<AShipBullet>ProjectileClass;
+
+	enum weaponType
+	{
+		cannon = 0, torpedo, aircraft, dCharge, aAir
+	};
 	
+	float currRotation;
+	float minRotation;
+	float maxRotation;
+	float minElevation;
+	float maxElevation;
+	float minRange;
+	float maxRange;
+	float weaponDamage;
+	float weaponAmmo;
+	float weaponAmmoMax;
+	float weaponFireSpeed;
+	float weaponReloadSpeed;
+	float accuracy;
+	bool inRange();
+	bool inFireArc();
 	void fireWeapon();
 	void reloadWeapon();
 	
