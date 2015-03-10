fabe6935e5bd258d2a9a254b5753652353155831
StevenCoverdale <S.coverdale@hotmail.com>
2015-03-10 15:54:35 -0400

    Additional Work
    Added team classes,
    changed Ships from ACTOR to PAWN

--- UnrealNavalBattles/Intermediate/Build/Win64/Inc/UnrealNavalBattles/UnrealNavalBattles.generated.cpp
+++ UnrealNavalBattles/Intermediate/Build/Win64/Inc/UnrealNavalBattles/UnrealNavalBattles.generated.cpp
@@ -56,7 +56,7 @@ void EmptyLinkFunctionForGeneratedCodeUnrealNavalBattles() {}
 	void AUNB_Ships::StaticRegisterNativesAUNB_Ships()
 	{
 	}
-	IMPLEMENT_CLASS(AUNB_Ships, 3130211529);
+	IMPLEMENT_CLASS(AUNB_Ships, 676063826);
 	void AUNB_SpectatorPawn::StaticRegisterNativesAUNB_SpectatorPawn()
 	{
 		FNativeFunctionRegistrar::RegisterFunction(AUNB_SpectatorPawn::StaticClass(),"OnRightClick",(Native)&AUNB_SpectatorPawn::execOnRightClick);
@@ -459,7 +459,7 @@ FName UNREALNAVALBATTLES_Fire = FName(TEXT("Fire"));
 		static UClass* OuterClass = NULL;
 		if (!OuterClass)
 		{
-			Z_Construct_UClass_AActor();
+			Z_Construct_UClass_APawn();
 			Z_Construct_UPackage_UnrealNavalBattles();
 			OuterClass = AUNB_Ships::StaticClass();
 			if (!(OuterClass->ClassFlags & CLASS_Constructed))
@@ -472,6 +472,7 @@ FName UNREALNAVALBATTLES_Fire = FName(TEXT("Fire"));
 				OuterClass->StaticLink();
 #if WITH_METADATA
 				UMetaData* MetaData = OuterClass->GetOutermost()->GetMetaData();
+				MetaData->SetValue(OuterClass, TEXT("HideCategories"), TEXT("Navigation AI|Navigation"));
 				MetaData->SetValue(OuterClass, TEXT("IncludePath"), TEXT("UNB_Ships.h"));
 				MetaData->SetValue(OuterClass, TEXT("ModuleRelativePath"), TEXT("UNB_Ships.h"));
 				MetaData->SetValue(NewProp__health, TEXT("Category"), TEXT("Health"));
@@ -598,7 +599,7 @@ FName UNREALNAVALBATTLES_Fire = FName(TEXT("Fire"));
 			ReturnPackage = CastChecked<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), NULL, FName(TEXT("/Script/UnrealNavalBattles")), false, false));
 			ReturnPackage->PackageFlags |= PKG_CompiledIn | 0x00000000;
 			FGuid Guid;
-			Guid.A = 0xF040339B;
+			Guid.A = 0x854AE2D2;
 			Guid.B = 0x8C4D86CD;
 			Guid.C = 0x00000000;
 			Guid.D = 0x00000000;
