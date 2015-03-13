11d3e168ae8bc183461048edf4e25ca4045d7ac1
Nolan Hunt <ninja-sora@hotmail.com>
2015-03-13 09:44:37 -0400

    fixed

--- UnrealNavalBattles/Source/UnrealNavalBattles/UNB_Timer.h
+++ UnrealNavalBattles/Source/UnrealNavalBattles/UNB_Timer.h
@@ -1,14 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-#include "UNB_Timer.generated.h
+
 /**
  * 
  */
-UCLASS()
+
 class UNREALNAVALBATTLES_API UNB_Timer
 {
-	GENERATED_BODY()
+	
 public:
 	UNB_Timer();
 	~UNB_Timer();
