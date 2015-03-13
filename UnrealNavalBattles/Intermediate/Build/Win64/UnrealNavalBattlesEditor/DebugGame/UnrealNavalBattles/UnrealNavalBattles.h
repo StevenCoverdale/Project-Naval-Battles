516e50a16d974f4defca6ba9e4df1a4a52abd3bc
Nolan Hunt <ninja-sora@hotmail.com>
2015-03-13 09:24:06 -0400

    test timer class

--- UnrealNavalBattles/Intermediate/Build/Win64/UnrealNavalBattlesEditor/DebugGame/UnrealNavalBattles/UnrealNavalBattles.h
+++ UnrealNavalBattles/Intermediate/Build/Win64/UnrealNavalBattlesEditor/DebugGame/UnrealNavalBattles/UnrealNavalBattles.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_UNREALNAVALBATTLES_H__
 #define __AUTO_UNREALNAVALBATTLES_H__
-//Last Write: 3/3/2015 5:25:11 PM +00:00
+//Last Write: 3/13/2015 12:45:08 PM +00:00
 #include "C:\Users\sctcad\Desktop\Project-Naval-Battles\UnrealNavalBattles\Source\UnrealNavalBattles\UnrealNavalBattles.h"
 #endif//__AUTO_UNREALNAVALBATTLES_H__
---
+++ UnrealNavalBattles/Source/UnrealNavalBattles/UNB_Timer.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealNavalBattles.h"
+#include "UNB_Timer.h"
+
+UNB_Timer::UNB_Timer()
+{
+}
+
+UNB_Timer::~UNB_Timer()
+{
+}
